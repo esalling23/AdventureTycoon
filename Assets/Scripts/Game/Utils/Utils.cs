5f653cefc89e05293378b39fe7852d2f907b15e6
Eron Salling <esalling23@gmail.com>
2024-08-12 13:06:02 -0400

    Adds utils class, LerpObject method

---
+++ Assets/Scripts/Game/Utils/Utils.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public static class Utils 
+{
+	public static IEnumerator LerpObject(Transform transform, Vector3 targetPosition, float duration)
+	{
+		float time = 0;
+		Vector3 startPosition = transform.position;
+
+		while (time < duration)
+		{
+				transform.position = Vector3.Lerp(startPosition, targetPosition, time / duration);
+				time += Time.deltaTime;
+				yield return null;
+		}
+		transform.position = targetPosition;
+	}
+}
