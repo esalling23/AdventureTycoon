549a7ff2d5cd5fbc084547ef480b1f7abeaa69da
Eron Salling <esalling23@gmail.com>
2024-08-12 13:27:14 -0400

    Updates map to use adjusted location classes

--- Assets/Scripts/Game/Map.cs
+++ Assets/Scripts/Game/Map.cs
@@ -1,5 +1,3 @@
-// using System.Numerics;
-// using System.Numerics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,129 +7,187 @@ using UnityEngine.EventSystems;
 
 public class Map : MonoBehaviour
 {
-    #region Fields
+	#region Fields
 
-		[SerializeField] private GameManager _manager;
+	[SerializeField] private GameManager _manager;
+	private CameraManager _camera;
+	public float cameraMoveSpeed = 50;
 
-		// Grid
-		public Vector2Int size;
-		public float cellSize = 10f;
-		private Grid<GridCell> _mapGrid;
+	// Grid
+	public Vector2Int gridSize;
+	public float cellSize = 10f;
+	private Grid<GridCell> _mapGrid;
 
-		// Locations
-		[SerializeField] private MapLocation[] _locationPrefabs;
+	// Locations
+	[SerializeField] private MapLocation _locationPrefab;
+	private Location _activeLocationToBuild;
 
-		// UI
-		[SerializeField] private GameObject _selectedIndicator;
-		[SerializeField] private LocationDetailsPanel _locationDetailsPanel;
+	// Adventurers
+	[SerializeField] private Adventurer _adventurerPrefab;
+	[SerializeField] private GameObject _adventurerContainer;
 
-		#endregion
+	// UI
+	[SerializeField] private GameObject _selectedIndicator;
+	[SerializeField] private LocationDetailsPanel _locationDetailsPanel;
 
-		#region Properties
+	#endregion
+
+	#region Properties
 
 
-		#endregion
+	#endregion
 
-		#region Methods
+	#region Methods
 
-    void Start()
-    {
-      _mapGrid = new Grid<GridCell>(
-				(int) size.x, 
-				(int) size.y, 
-				cellSize, 
-				new Vector2(-size.x * 5, -size.y * 5),
-				(Grid<GridCell> g, int x, int y) => new GridCell(g, new Vector2Int(x, y))
-			);
+	void Start()
+	{
+		_camera = GameObject.FindWithTag("MainCamera").GetComponent<CameraManager>();
 
-			InitMapLocations(_locationPrefabs, 3);
-    }
+		_mapGrid = new Grid<GridCell>(
+			(int) gridSize.x, 
+			(int) gridSize.y, 
+			cellSize, 
+			new Vector2(-gridSize.x * 5, -gridSize.y * 5),
+			(Grid<GridCell> g, int x, int y) => new GridCell(g, new Vector2Int(x, y))
+		);
 
-    void Update()
-    {
-			if (!EventSystem.current.IsPointerOverGameObject() && Input.GetMouseButtonDown(0)) {
-				GridCell cell = _mapGrid.GetGridObject(UtilsClass.GetMouseWorldPosition());
-				Debug.Log($"Clicked on cell coordinates ({cell.Coordinates.x}, {cell.Coordinates.y})");
-				switch(_manager.Mode) {
-					case GameMode.Build:
-						PlaceLocation(cell, GetRandomLocationPrefab(_locationPrefabs));
-						break;
-					default: 
-						break;
-				}
-				SelectCell(cell);
+		_activeLocationToBuild = null;
+
+		InitMapLocations(DataManager.Instance.WorldLocations, 3);
+		InitMapAdventurers(2);
+	}
+
+	void Update()
+	{
+		if (!EventSystem.current.IsPointerOverGameObject() && Input.GetMouseButtonDown(0)) {
+			GridCell cell = _mapGrid.GetGridObject(UtilsClass.GetMouseWorldPosition());
+			Debug.Log($"Clicked on cell coordinates ({cell.Coordinates.x}, {cell.Coordinates.y})");
+			switch(_manager.Mode) {
+				case GameMode.Build:
+					if (_activeLocationToBuild != null) {
+						PlaceLocation(cell, _activeLocationToBuild);
+					}
+					else 
+					{
+						Debug.Log("No Location Selected For Building");
+					}
+					break;
+				default: 
+					break;
 			}
+			SelectCell(cell);
+		}
 
-			if (Input.GetMouseButtonDown(1)) {
-				Deselect();
-			}
-    }
+		if (Input.GetMouseButtonDown(1)) {
+			Deselect();
+		}
+	}
 
-		private void Deselect() {
-			_selectedIndicator.SetActive(false);
+	public MapLocation FindLocationWithActivityType(ActivityType type) 
+	{
+		return new MapLocation();
+		// return WorldLocations.First();
+	}
+
+	private void Deselect() {
+		_selectedIndicator.SetActive(false);
+		_locationDetailsPanel.gameObject.SetActive(false);
+	}
+
+	public void SetLocationToBuild(Location location) 
+	{
+		_manager.SetMode(GameMode.Build);
+		_activeLocationToBuild = location;
+	}
+	
+	private void SelectCell(GridCell cell) {
+		Vector3 centeredCellPos = _mapGrid.GetCenteredCellPosition(cell.Coordinates.x, cell.Coordinates.y);
+		
+		_selectedIndicator.transform.position = centeredCellPos;
+		_selectedIndicator.SetActive(true);
+
+		//todo - move to it's own camera movement script!
+		_camera.AnimateTo(centeredCellPos, 0.5f);
+		
+		if (cell.Location) {
+			_locationDetailsPanel.SetLocationData(cell.Location);
+			_locationDetailsPanel.gameObject.SetActive(true);
+		} else {
 			_locationDetailsPanel.gameObject.SetActive(false);
 		}
+	}
 
-		private void SelectCell(GridCell cell) {
-			_selectedIndicator.transform.position = _mapGrid.GetCenteredCellPosition(cell.Coordinates.x, cell.Coordinates.y);
-			_selectedIndicator.SetActive(true);
+	public void PlaceLocation(GridCell cell, Location locationData) {
+		// only place if it's a null position
+		if (!cell?.Location) {
+			// Create the actual location object to display (visual layer)
+			MapLocation newPlacement = CreateLocationObject(locationData, cell);
+			newPlacement.SetSpriteSize((float) cellSize / 2, (float) cellSize / 2);
+			// Keep track of the placed object in the grid (data later)
+			cell.PlaceLocation(newPlacement);
+			// Todo: Trigger grid update event
+			_mapGrid.TriggerOnChangeEvent(cell.Coordinates);
 
-			if (cell.Location) {
-				_locationDetailsPanel.SetLocationData(cell.Location);
-				_locationDetailsPanel.gameObject.SetActive(true);
-			} else {
-				_locationDetailsPanel.gameObject.SetActive(false);
-			}
+			_activeLocationToBuild = null;
+			_manager.SetMode(GameMode.Run);
 		}
+	}
 
-		public void PlaceLocation(GridCell cell, MapLocation locationPrefab) {
-			// only place if it's a null position
-			if (!cell?.Location) {
-				// Create the actual location object to display (visual layer)
-				MapLocation newPlacement = CreateLocationObject(locationPrefab, cell);
-				newPlacement.SetSpriteSize((float) cellSize / 2, (float) cellSize / 2);
-				// Keep track of the placed object in the grid (data later)
-				cell.PlaceLocation(newPlacement);
-				// Todo: Trigger grid update event
-				_mapGrid.TriggerOnChangeEvent(cell.Coordinates);
-			}
-		}
+	public void Regenerate() {
+		// InitMapLocations();
+	}
 
-		public void Regenerate() {
-			// InitMapLocations();
-		}
+	private void GenerateMapTerrain() {
+		// generate map background/terrain
+	}
 
-		private void GenerateMapTerrain() {
-			// generate map background/terrain
-		}
+	public Location GetRandomLocationData(Location[] available) {
+		int rand = Random.Range(0, available.Length);
+		return available[rand];
+	}
 
-		private MapLocation GetRandomLocationPrefab(MapLocation[] available) {
-			int rand = Random.Range(0, available.Length);
-			return available[rand];
-		}
-
-		private void InitMapLocations(MapLocation[] available, int count) {
-			for (int i = 0; i < count; i++) {	
-				// find empty cell			
-				GridCell cell;
-				do {
-					cell = _mapGrid.GetRandomGridObject();
-				} while (cell?.Location != null);
-
-				// init locations are random
-				MapLocation randLocation = GetRandomLocationPrefab(available);
-				PlaceLocation(cell, randLocation);
-			}
-		}
-
-		private MapLocation CreateLocationObject(MapLocation locationPrefab, GridCell cell) {
-			return Instantiate(
-				locationPrefab,
-				_mapGrid.GetCenteredCellPosition(cell.Coordinates.x, cell.Coordinates.y),
-				Quaternion.identity, 
-				this.transform
+	private void InitMapAdventurers(int count) {
+		for (int i = 0; i < count; i++) {
+			Adventurer newAdventurer = Instantiate(
+				_adventurerPrefab,
+				Vector3.zero,
+				Quaternion.identity,
+				_adventurerContainer.transform
 			);
+			// newAdventurer.
 		}
+	}
 
-		#endregion
+	private void InitMapLocations(Location[] available, int count) {
+		int locCount = count;
+		if (count > _mapGrid.TotalCellCount) {
+			locCount = _mapGrid.TotalCellCount;
+		}
+		for (int i = 0; i < locCount; i++) {	
+			// find empty cell			
+			GridCell cell;
+			do {
+				cell = _mapGrid.GetRandomGridObject();
+			} while (cell?.Location != null);
+
+			// init locations are random
+			Location randLocation = GetRandomLocationData(available);
+			PlaceLocation(cell, randLocation);
+		}
+	}
+
+	private MapLocation CreateLocationObject(Location locationData, GridCell cell) {
+		MapLocation location = Instantiate(
+			_locationPrefab,
+			_mapGrid.GetCenteredCellPosition(cell.Coordinates.x, cell.Coordinates.y),
+			Quaternion.identity, 
+			this.transform
+		);
+
+		location.SetData(locationData);
+		
+		return location; 
+	}
+
+	#endregion
 }
