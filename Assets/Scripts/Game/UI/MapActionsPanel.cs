4ab1336ea190b8c62c17944290ec8c9537e66e34
Eron Salling <esalling23@gmail.com>
2024-08-12 13:25:28 -0400

    Adds build location generation for map actions panel

--- Assets/Scripts/Game/UI/MapActionsPanel.cs
+++ Assets/Scripts/Game/UI/MapActionsPanel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Linq;
 
 public class MapActionsPanel : MonoBehaviour
 {
@@ -10,6 +11,11 @@ public class MapActionsPanel : MonoBehaviour
 
 		[SerializeField] private Map _map;
 		[SerializeField] private GameManager _manager;
+
+		[SerializeField] private int _locationGenerationCount = 3;
+
+		[SerializeField] private GameObject _buildShelf;
+		[SerializeField] private LocationDetailItem _locationDetailPrefab;
 
 
 		#endregion
@@ -22,15 +28,50 @@ public class MapActionsPanel : MonoBehaviour
 
     void Start()
     {
+			ClearBuildShelf();
+			HideBuildShelf();
+
+			EventManager.StartListening(EventName.OnBuildLocationSelected, HandleBuildLocationSelected);
     }
 
-		// public void Toggle
+		void HandleBuildLocationSelected(Dictionary<string, object> data) 
+		{
+			HideBuildShelf();
+		}
 
+		void ClearBuildShelf() {
+			foreach (Transform child in _buildShelf.transform) {
+				Destroy(child.gameObject);
+			}
+		}
+
+		public void HideBuildShelf() {
+			_buildShelf.SetActive(false);
+		}
 		public void HandleClickBuildMode() {
-			if (_manager.Mode == GameMode.Build) {
-				_manager.SetMode(GameMode.Run);
-			};
-			_manager.SetMode(GameMode.Build);
+			ClearBuildShelf();
+			// generate # of locations for player
+			Location[] options = new Location[_locationGenerationCount];
+			List<Location> availableLocations = new List<Location>(DataManager.Instance.WorldLocations);
+
+			for (int i = 0; i < _locationGenerationCount; i++) 
+			{
+				Location newLocation = _map.GetRandomLocationData(availableLocations.ToArray());
+
+				availableLocations.Remove(newLocation);
+
+				// and display them
+				LocationDetailItem item = Instantiate(
+					_locationDetailPrefab,
+					Vector3.zero,
+					Quaternion.identity, 
+					_buildShelf.transform
+				);
+				item.gameObject.transform.position = Vector3.zero;
+				item.SetData(newLocation);
+			}
+
+			_buildShelf.SetActive(true);
 		}
 
 		// public void HandleClickTimingButton(TimeChangeType type) {
@@ -39,6 +80,8 @@ public class MapActionsPanel : MonoBehaviour
 			// };
 			// _manager.SetMode(GameMode.Build);
 		// }
+
+
 
 		#endregion
 }
